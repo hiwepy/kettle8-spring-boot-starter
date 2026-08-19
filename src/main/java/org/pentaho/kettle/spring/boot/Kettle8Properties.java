b069ac9bf68be9ce0c09380dd3a32ada01c2b456
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-19 20:19:28 +0800

    fix: add comprehensive English Javadoc

--- src/main/java/org/pentaho/kettle/spring/boot/Kettle8Properties.java
+++ src/main/java/org/pentaho/kettle/spring/boot/Kettle8Properties.java
@@ -3,6 +3,8 @@ package org.pentaho.kettle.spring.boot;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
+ * @since 1.0.0
  */
 @ConfigurationProperties(Kettle8Properties.PREFIX)
 public class Kettle8Properties {
